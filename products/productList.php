1d36706e620959fb1eca226d7e0b3a4d5eee56d5
aythameitor <aythamicm@gmail.com>
2023-05-19 18:06:43 +0000

    another small fix

--- products/productList.php
+++ products/productList.php
@@ -21,10 +21,11 @@ try {
                     $cart =& $_SESSION["cart"];
                     $selfUserId = select("userId", strip_tags(trim($_POST["email"])), $connection)->fetchColumn();
                     foreach ($cart as $key => $value) {
-                        $productHistoryQuery = 'insert into productHistory(userId, productId) values(:userId, :productId)';
+                        $productHistoryQuery = 'INSERT INTO productHistory (userId, productId, saleDate) VALUES (:userId, :productId, :saleDate)';
                         $productHistory = $connection->prepare($productHistoryQuery);
                         $productHistory->bindParam(":userId", $selfUserId);
                         $productHistory->bindParam(":productId", $value);
+                        $productHistory->bindValue(":saleDate", date('Y-m-d'));
                         $productHistory->execute();
                         unset($cart[$key]);
                     }
@@ -114,8 +115,7 @@ if (isset($success)) {
     ?>
     <div>
         <div style="width:100%; display:flex;justify-content:center">
-            <span
-                class="successMsg">
+            <span class="successMsg">
                 <?= $success ?>
             </span>
         </div>
@@ -141,123 +141,124 @@ if (isset($error)) {
 <div class="container">
     <div class="row">
 
-    <h2 class="p-2">Product list</h2>
-    
-    <?php
-    if ($product && $sentencia->rowCount() > 0) {
-        foreach ($product as $row) {
-            ?>
-            <div class="card" style="width: 18rem; margin:10px;">
-                <img style="width:100%;height:262px;object-fit:cover" class="card-img-top" src='<?php echo $row["picture"]; ?>'>
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <?php
-                        if (isset($_SESSION["roleId"]) && $_SESSION["roleId"] > 1) {
-                            echo "Product id: " . $row["productId"];
-                        }
-                        ?>
-                        <?php echo "<br>" . "<strong>Name:</strong> " . $row["name"]; ?>
+        <h2 class="p-2">Product list</h2>
 
-                    </h5>
-                    <p class="card-text">
-                        <strong>Release date:</strong>
-                        <?php echo $row["releaseDate"]; ?> <br>
-                        <strong>Price:</strong>
-                        <?php echo $row["price"] . "€"; ?> <br>
-                        <strong>Description:</strong>
-                        <?php echo $row["description"]; ?> <br>
-                        <strong>Stock:</strong>
-                        <?php echo $row["stock"]; ?> <br>
-                        <strong>Product type:</strong>
-                        <?php
-                        $rol = 'select productType from productType where productTypeId=' . $row["productTypeId"];
-                        $consultaRol = $connection->prepare($rol);
-                        $consultaRol->execute();
-                        $nombreRol = $consultaRol->fetchColumn();
-                        echo $nombreRol;
-                        ?> <br>
-
-                    <div style="display:flex;justify-content:center">
-                        <?php
-                        if (isset($_SESSION["roleId"]) && $_SESSION["roleId"] > 1) {
+        <?php
+        if ($product && $sentencia->rowCount() > 0) {
+            foreach ($product as $row) {
+                ?>
+                <div class="card" style="width: 18rem; margin:10px;">
+                    <img style="width:100%;height:262px;object-fit:cover" class="card-img-top"
+                        src='<?php echo $row["picture"]; ?>'>
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?php
+                            if (isset($_SESSION["roleId"]) && $_SESSION["roleId"] > 1) {
+                                echo "Product id: " . $row["productId"];
+                            }
                             ?>
-                            <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg"
-                                style="display:flex;justify-content:center;align-items:center">
-                                <a href="productList.php?delete=<?php echo $row["productId"]; ?>">
-                                    <image href="/images/svg/trash-can.svg" height="25" width="25">
-                                </a>
-                            </svg>
+                            <?php echo "<br>" . "<strong>Name:</strong> " . $row["name"]; ?>
+
+                        </h5>
+                        <p class="card-text">
+                            <strong>Release date:</strong>
+                            <?php echo $row["releaseDate"]; ?> <br>
+                            <strong>Price:</strong>
+                            <?php echo $row["price"] . "€"; ?> <br>
+                            <strong>Description:</strong>
+                            <?php echo $row["description"]; ?> <br>
+                            <strong>Stock:</strong>
+                            <?php echo $row["stock"]; ?> <br>
+                            <strong>Product type:</strong>
+                            <?php
+                            $rol = 'select productType from productType where productTypeId=' . $row["productTypeId"];
+                            $consultaRol = $connection->prepare($rol);
+                            $consultaRol->execute();
+                            $nombreRol = $consultaRol->fetchColumn();
+                            echo $nombreRol;
+                            ?> <br>
+
+                        <div style="display:flex;justify-content:center">
+                            <?php
+                            if (isset($_SESSION["roleId"]) && $_SESSION["roleId"] > 1) {
+                                ?>
+                                <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg"
+                                    style="display:flex;justify-content:center;align-items:center">
+                                    <a href="productList.php?delete=<?php echo $row["productId"]; ?>">
+                                        <image href="/images/svg/trash-can.svg" height="25" width="25">
+                                    </a>
+                                </svg>
                                 <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg"
                                     style="display:flex;justify-content:center;align-items:center">
                                     <a href="productEdit.php?product=<?php echo $row["productId"]; ?>">
                                         <image href="/images/svg/pen.svg" height="25" width="25">
                                     </a>
                                 </svg>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                        <div style="display:flex;justify-content:space-between">
                             <?php
-                        }
-                        ?>
-                    </div>
-                    <div style="display:flex;justify-content:space-between">
-                    <?php
-                    if (isset($_SESSION["roleId"]) && $_SESSION["roleId"] < 2) {
-                        $checkWishQuery = "select * from wishlist where productId = :productId and userId = :userId";
-                        $checkWish = $connection->prepare($checkWishQuery);
-                        $checkWish->bindParam(":productId", $row['productId']);
-                        $idCheckWish = select("userId", $_SESSION["email"], $connection)->fetchColumn();
-                        $checkWish->bindParam(":userId", $idCheckWish);
-                        $checkWish->execute();
-                        if ($checkWish->rowCount() > 0) {
+                            if (isset($_SESSION["roleId"]) && $_SESSION["roleId"] < 2) {
+                                $checkWishQuery = "select * from wishlist where productId = :productId and userId = :userId";
+                                $checkWish = $connection->prepare($checkWishQuery);
+                                $checkWish->bindParam(":productId", $row['productId']);
+                                $idCheckWish = select("userId", $_SESSION["email"], $connection)->fetchColumn();
+                                $checkWish->bindParam(":userId", $idCheckWish);
+                                $checkWish->execute();
+                                if ($checkWish->rowCount() > 0) {
+                                    ?>
+
+                                    <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg"
+                                        style="display:flex;justify-content:center;align-items:center">
+                                        <a href="productList.php?wishlist=<?php echo $row["productId"]; ?>">
+                                            <image href="/images/svg/loved-product.svg" height="25" width="25">
+                                        </a>
+                                    </svg>
+
+                                    <?php
+                                } else {
+                                    ?>
+                                    <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg"
+                                        style="display:flex;justify-content:center;align-items:center">
+                                        <a href="productList.php?wishlist=<?php echo $row["productId"]; ?>">
+                                            <image href="/images/svg/love.svg" height="25" width="25">
+                                        </a>
+                                    </svg>
+
+                                    <?php
+                                }
+
+                            }
                             ?>
+                            <?php
+                            if (isset($_SESSION["roleId"]) && $_SESSION["roleId"] < 2) {
+                                ?>
 
-                            <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg"
-                                style="display:flex;justify-content:center;align-items:center">
-                                <a href="productList.php?wishlist=<?php echo $row["productId"]; ?>">
-                                    <image href="/images/svg/loved-product.svg" height="25" width="25">
-                                </a>
-                            </svg>
+                                <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg"
+                                    style="display:flex;justify-content:center;align-items:center">
+                                    <a href="productList.php?cart=<?php echo $row["productId"]; ?>">
+                                        <image href="/images/svg/cart.svg" height="25" width="25">
+                                    </a>
+                                </svg>
 
-                        <?php
-                        } else {
+                                <?php
+                            }
                             ?>
-                            <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg"
-                                style="display:flex;justify-content:center;align-items:center">
-                                <a href="productList.php?wishlist=<?php echo $row["productId"]; ?>">
-                                    <image href="/images/svg/love.svg" height="25" width="25">
-                                </a>
-                            </svg>
+                        </div>
+                        </p>
 
-                        <?php
-                        }
-                        
-                    }
-                    ?>
-                    <?php
-                if (isset($_SESSION["roleId"]) && $_SESSION["roleId"] < 2) {
-                    ?>
-
-                        <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg"
-                            style="display:flex;justify-content:center;align-items:center">
-                            <a href="productList.php?cart=<?php echo $row["productId"]; ?>">
-                                <image href="/images/svg/cart.svg" height="25" width="25">
-                            </a>
-                        </svg>
-
-                    <?php
-                }
-                ?>
                     </div>
-                    </p>
-
                 </div>
-            </div>
 
-            <?php
+                <?php
+            }
         }
-    }
 
-    ?>
-        
-        </div>
+        ?>
+
+    </div>
 </div>
 
 
